fc20299fe6ea8194f04b02bf600ce6e914f31591
Ramón Gavira <ramgavsan@alum.us.es>
2024-11-13 16:25:03 +0100

    cambios para preparar el despliegue

--- pueblista/pueblista/settings.py
+++ pueblista/pueblista/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.onrender.com','https://pueblista-pgpi.onrender.com']
 
 
 # REDIRECT TO LOGIN PAGE IF NOT LOGGED IN
