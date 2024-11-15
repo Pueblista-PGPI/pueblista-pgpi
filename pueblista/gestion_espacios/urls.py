926a589314e7734bfadfe1926ea3904bf2223aa7
Ramón Gavira <ramgavsan@alum.us.es>
2024-11-15 10:15:49 +0100

    feat: Arreglar redireccionamientos en homepage

--- pueblista/gestion_espacios/urls.py
+++ pueblista/gestion_espacios/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from gestion_espacios import views
 
-
 urlpatterns = [
     path('delete/<int:id>/', views.delete, name='delete'),
     path('edit/<int:id>/', views.edit, name='edit'),
