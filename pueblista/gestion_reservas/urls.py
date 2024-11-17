83a15f44030d63803e9abacb8e8dd00b2cc3827a
DaniFdezCab <danfercab@alum.us.es>
2024-11-17 18:28:40 +0100

    feat: Cancelación de reservas e implementación de mensajes de control
    La implementación de los mensajes de control se han realizado en la propia pantalla de calendario_reservas, aunque es recomendable meterlos en base.html
    
    Co-authored-by: José Miguel <josemicpy@users.noreply.github.com>

--- pueblista/gestion_reservas/urls.py
+++ pueblista/gestion_reservas/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from gestion_reservas import views
 
 urlpatterns = [
-    # path('delete/<int:id>/', views.delete, name='delete'),
-    # path('edit/<int:id>/', views.edit, name='edit'),
     path('', views.calendario_reservas, name='calendario_reservas'),
     path('crear/', views.crear_reserva, name='crear_reserva'),
-    # path('create/', views.create, name='create'),
+    path('cancelar/<int:id>/', views.cancelar_reserva, name='cancelar_reserva'),
 ]
+
--- pueblista/gestion_reservas/views.py
+++ pueblista/gestion_reservas/views.py
@@ -4,6 +4,7 @@ from .models import Reserva
 from gestion_espacios.models import EspacioPublico
 from datetime import datetime
 from django.contrib.sessions.models import Session
+from django.contrib import messages
 
 
 @login_required
@@ -44,33 +45,60 @@ def calendario_reservas(request, id):
         'horarios_reservas': horarios_reservas,
         'espacio': espacio,
         'nombre_completo': request.user.nombre + ' ' + request.user.apellidos,
+        'reserva_id': -1,
     })
 
 @login_required
 def crear_reserva(request, id):
-    if request.method == 'POST':
+    espacio = '#'
+    try:
         espacio = get_object_or_404(EspacioPublico, id=id)
-        fecha = request.POST.get('fecha')
-        hora_inicio = request.POST.get('hora_inicio')
-        hora_fin = request.POST.get('hora_fin')
-        request.session['fecha'] = fecha
-        
-        # Validar si ya existe una reserva en este intervalo
-        if Reserva.objects.filter(
-            espacio=espacio,
-            fecha=fecha,
-            hora_inicio=hora_inicio,
-            hora_fin=hora_fin
-        ).exists():
-            return redirect('calendario_reservas', id=espacio.id)  # Manejo de conflicto
-        
-        # Crear la reserva
-        Reserva.objects.create(
-            espacio=espacio,
-            usuario=request.user,
-            fecha=fecha,
-            hora_inicio=hora_inicio,
-            hora_fin=hora_fin
-        )
-        
-        return redirect('calendario_reservas', id=espacio.id)
+        if request.method == 'POST':
+            fecha = request.POST.get('fecha')
+            hora_inicio = request.POST.get('hora_inicio')
+            hora_fin = request.POST.get('hora_fin')
+            request.session['fecha'] = fecha
+            
+            # Validar si ya existe una reserva en este intervalo
+            if not Reserva.objects.filter(
+                espacio=espacio,
+                fecha=fecha,
+                hora_inicio=hora_inicio,
+                hora_fin=hora_fin
+            ).exists():
+            # Crear la reserva
+                Reserva.objects.create(
+                    espacio=espacio,
+                    usuario=request.user,
+                    fecha=fecha,
+                    hora_inicio=hora_inicio,
+                    hora_fin=hora_fin
+                )
+                messages.success(request, "La reserva se ha creado exitosamente.")
+            else:
+                messages.error(request, "Ya existe una reserva en este intervalo.")
+        return redirect('calendario_reservas', id=espacio.id) 
+    except Exception as e:
+        messages.error(request, f"Ocurrió un error al crear la reserva: {str(e)}")
+        return redirect('calendario_reservas', id=espacio)
+
+            
+
+@login_required
+def cancelar_reserva(request, id):
+    # Intenta obtener la reserva asociada al usuario actual
+    reserva = get_object_or_404(Reserva, id=id, usuario=request.user)
+    espacio_id = reserva.espacio.id
+
+    try:
+        # Elimina la reserva
+        reserva.delete()
+        # Muestra un mensaje de éxito al usuario
+        messages.success(request, "La reserva se ha cancelado exitosamente.")
+    except Exception as e:
+        # Si ocurre un error, muestra un mensaje de error
+        messages.error(request, f"Ocurrió un error al cancelar la reserva: {str(e)}")
+    
+    return redirect('calendario_reservas', id=espacio_id)
+
+    # Redirige al calendario de reservas
