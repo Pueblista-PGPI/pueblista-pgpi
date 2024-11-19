fd5b07e850c469dc5c52e149a864edf24171eee8
Antonio Macías <antmacfer1@alum.us.es>
2024-11-19 23:26:23 +0100

    feat: listado de reservas por administrador hecho
    El buscador todavía no funciona

--- pueblista/listado_reservas/urls.py
+++ pueblista/listado_reservas/urls.py
@@ -3,6 +3,10 @@ from listado_reservas import views
 
 urlpatterns = [
     path('', views.listado_reservas, name='listado_reservas'),
-    path('eliminar_reserva/<int:reserva_id>/', views.eliminar_reserva, name='eliminar_reserva'),
-    path('modificar_reserva/<int:reserva_id>/', views.modificar_reserva, name='modificar_reserva'),
+    path('eliminar_reserva/<int:reserva_id>/',
+         views.eliminar_reserva, name='eliminar_reserva'),
+    path('modificar_reserva/<int:reserva_id>/',
+         views.modificar_reserva, name='modificar_reserva'),
+    path('modificar_estado/<int:reserva_id>/',
+         views.modificar_estado, name='modificar_estado'),
 ]
--- pueblista/listado_reservas/views.py
+++ pueblista/listado_reservas/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from datetime import datetime
 
+
 @login_required
 def listado_reservas(request):
     usuario = request.user
@@ -28,10 +29,14 @@ def listado_reservas(request):
 
     return render(request, 'listado_reservas.html', {"reservas": reservas, "query": query})
 
+
 @login_required
 def eliminar_reserva(request, reserva_id):
     # Intenta obtener la reserva asociada al usuario actual
-    reserva = get_object_or_404(Reserva, id=reserva_id, usuario=request.user)
+    if request.user.tipo_usuario == 'personal_administrativo' or request.user.tipo_usuario == 'superusuario':
+        reserva = get_object_or_404(Reserva, id=reserva_id)
+    else:
+        reserva = get_object_or_404(Reserva, id=reserva_id, usuario=request.user)
 
     try:
         # Elimina la reserva
@@ -41,8 +46,9 @@ def eliminar_reserva(request, reserva_id):
     except Exception as e:
         # Si ocurre un error, muestra un mensaje de error
         messages.error(request, f"Ocurrió un error al cancelar la reserva: {str(e)}")
-    
+
     return redirect('listado_reservas')
+
 
 @login_required
 def modificar_reserva(request, reserva_id):
@@ -87,3 +93,16 @@ def modificar_reserva(request, reserva_id):
         else:
             messages.error(request, "Ya existe una reserva en este intervalo.")
         return redirect('listado_reservas')
+
+
+@login_required
+def modificar_estado(request, reserva_id):
+    reserva = get_object_or_404(Reserva, id=reserva_id)
+    espacio_id = reserva.espacio.id
+    if request.method == 'POST':
+        estado = request.POST.get('estado')
+        reserva.estado = estado
+        reserva.save()
+        messages.success(request, "El estado de la reserva se ha modificado exitosamente.")
+        return redirect(f'/espacios/reservas/{espacio_id}/')
+    return render(request, 'modificar_estado.html', {'reserva': reserva})
